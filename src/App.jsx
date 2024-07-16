980a50aedf47a2a39dfb090aa4902d0ec526c734
TangaroAljun <aljunitotangaro9@gmail.com>
2024-07-16 22:06:06 +0800

    added funtions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Student Management System</h1>
+        <h1>Skwelahan</h1>
         <Students />
       </header>
     </div>
